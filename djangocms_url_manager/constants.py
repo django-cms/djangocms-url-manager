e54c46a2f7c5c62da3bf56475af44b22ca0c7ad2
vThaian <b.plociennik@merixstudio.com>
2018-09-05 15:22:50 +0200

    Add constant for select2 content object

--- djangocms_url_manager/constants.py
+++ djangocms_url_manager/constants.py
@@ -1,3 +1,5 @@
 PLUGIN_URL_NAME_PREFIX = 'djangocms_url_manager'
 
-SELECT2_PAGE_URL_NAME = '{}_select2_page'.format(PLUGIN_URL_NAME_PREFIX)
+SELECT2_CONTENT_TYPE_OBJECT_URL_NAME = '{}_select2_content_type_object'.format(
+    PLUGIN_URL_NAME_PREFIX
+)
