fb4c1071aafd72c0380b5e6b0fd722b7b123cb6b
adam-murray <adam-murray@hotmail.co.uk>
2020-06-02 15:48:56 +0100

    Linting

--- djangocms_url_manager/models.py
+++ djangocms_url_manager/models.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.sites.models import Site
-from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
