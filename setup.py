8482fda818569bc37441ae49c276edad266e5d44
Andrew Aikman <andrew.aikman@eliga.services>
2021-02-03 19:06:39 +0000

    Fix the test suite

--- setup.py
+++ setup.py
@@ -6,12 +6,7 @@ import djangocms_url_manager
 INSTALL_REQUIREMENTS = [
     "Django>=1.11,<3.0",
     "django-cms",
-    "djangocms-attributes-field>=0.1.1",
-]
-
-TEST_REQUIREMENTS = [
-    "djangocms_helper",
-    "djangocms-versioning",
+    "djangocms-attributes-field<2.0.0",  # django 1.11 compat
 ]
 
 
@@ -30,14 +25,9 @@ setup(
         "Topic :: Software Development",
     ],
     install_requires=INSTALL_REQUIREMENTS,
-    tests_require=TEST_REQUIREMENTS,
     author="Divio AG",
     author_email="info@divio.ch",
     url="http://github.com/divio/djangocms-url-manager",
     license="BSD",
     test_suite="test_settings.run",
-    dependency_links=[
-        "http://github.com/divio/django-cms/tarball/release/4.0.x#egg=django-cms-4.0.0",
-        "http://github.com/divio/djangocms-versioning/tarball/master#egg=djangocms-versioning-0.0.23",
-    ]
 )
