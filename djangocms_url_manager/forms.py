1bfacc262fc1cdf07c7b5221bf2887e70fd51d59
bernard <bvdv9075@gmail.com>
2021-11-12 15:56:54 +0000

    removed unused imports

--- djangocms_url_manager/forms.py
+++ djangocms_url_manager/forms.py
@@ -7,7 +7,7 @@ from django.utils.translation import ugettext_lazy as _
 from cms.utils.urlutils import admin_reverse
 
 from .constants import SELECT2_CONTENT_TYPE_OBJECT_URL_NAME, SELECT2_URLS
-from .models import BASIC_TYPE_CHOICES, LinkPlugin, Url, UrlOverride, UrlGrouper
+from .models import BASIC_TYPE_CHOICES, LinkPlugin, Url, UrlOverride
 from .utils import supported_models
 
 
