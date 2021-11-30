259e062fa8e504513d3170482b9a001b2fff6156
Adam Murray <adam-murray@hotmail.co.uk>
2021-11-30 19:01:33 +0000

    Fix: Added edit check, and moved check for none

--- djangocms_url_manager/templatetags/djangocms_url_manager_tags.py
+++ djangocms_url_manager/templatetags/djangocms_url_manager_tags.py
@@ -10,6 +10,13 @@ register = template.Library()
 def render_url(context, instance):
     request = context["request"]
     toolbar = get_toolbar_from_request(request)
-    url = instance.get_content(toolbar.preview_mode_active)
+    show_draft_content = False
+    if toolbar.edit_mode_active or toolbar.preview_mode_active:
+        show_draft_content = True
+
+    url = instance.get_content(show_draft_content)
+
+    if not url:
+        return ""
     renderer = toolbar.get_content_renderer()
     return url.get_url(renderer.current_site) or ""
